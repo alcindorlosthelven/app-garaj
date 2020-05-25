4858c5593039c1974c6b1cb8916a9129f0a6eafa
los <alcindorlos@gmail.com>
2020-05-24 23:03:28 -0400

    xxx

--- app/DefaultApp/Vues/block/menu.php
+++ app/DefaultApp/Vues/block/menu.php
@@ -152,7 +152,7 @@ use systeme\Application\Application as App;
                 <li class="nav-item">
                     <a href="<?= App::genererUrl("employer") ?>" class="nav-link">
                         <i class="nav-icon far fa-user text-info"></i>
-                        <p>Employerssss</p>
+                        <p>Employer</p>
                     </a>
                 </li>
 
